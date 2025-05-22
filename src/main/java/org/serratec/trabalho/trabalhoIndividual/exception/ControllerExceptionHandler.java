de7e5e6df0f2e53253e5cc7bd5a8625ab9eb3710
João Pedro Dias Rodrigues <joaopedrodias890@gmail.com>
2025-05-22 13:35:38 -0300

    Classe ControllerExceptionHandler criada

---
+++ src/main/java/org/serratec/trabalho/trabalhoIndividual/exception/ControllerExceptionHandler.java
@@ -0,0 +1,32 @@
+package org.serratec.trabalho.trabalhoIndividual.exception;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		
+		List<String> erros = new ArrayList<>();
+		for(FieldError er: ex.getBindingResult().getFieldErrors()) {
+			erros.add(er.getField() + ": " + er.getDefaultMessage());
+		}
+		
+		return super.handleExceptionInternal(ex, erros, headers, status, request);
+	}
+
+	
+}
