e276dec52f0b755f64ef05edf9025bb0e39fe49d
João Pedro Dias Rodrigues <joaopedrodias890@gmail.com>
2025-05-22 13:41:25 -0300

    Criação de novo erro

--- src/main/java/org/serratec/trabalho/trabalhoIndividual/exception/ControllerExceptionHandler.java
+++ src/main/java/org/serratec/trabalho/trabalhoIndividual/exception/ControllerExceptionHandler.java
@@ -1,15 +1,18 @@
 package org.serratec.trabalho.trabalhoIndividual.exception;
 
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -28,5 +31,10 @@ public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
 		return super.handleExceptionInternal(ex, erros, headers, status, request);
 	}
 
+	 @ExceptionHandler(Exception.class)
+	    public ResponseEntity<ErroResposta> handleGeneric(Exception ex) {
+	        ErroResposta resposta = new ErroResposta(LocalDateTime.now(), HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.getMessage(), "/funcionarios");
+	        return new ResponseEntity<>(resposta, HttpStatus.INTERNAL_SERVER_ERROR);
+	    }
 	
 }
